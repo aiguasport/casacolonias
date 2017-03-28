446e6666251b99fcf26b2fa46b6d0f5db41302a4
aiguasport <aiguasportproyecto@gmail.com>
2017-03-28 22:36:51 +0200

    V71.1

--- Presentacion/AddTareas.Designer.cs
+++ Presentacion/AddTareas.Designer.cs
@@ -36,6 +36,13 @@
             this.txtCodigo = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.txtNivel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbCasas = new System.Windows.Forms.ComboBox();
+            this.btnTerminar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listCasas = new System.Windows.Forms.ListView();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -62,18 +69,17 @@
             // txtDescripcion
             // 
             this.txtDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDescripcion.Location = new System.Drawing.Point(37, 235);
+            this.txtDescripcion.Location = new System.Drawing.Point(341, 155);
             this.txtDescripcion.Multiline = true;
             this.txtDescripcion.Name = "txtDescripcion";
             this.txtDescripcion.Size = new System.Drawing.Size(275, 26);
             this.txtDescripcion.TabIndex = 27;
-            this.txtDescripcion.Text = "6666666666666666666666";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(33, 212);
+            this.label2.Location = new System.Drawing.Point(337, 132);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(136, 20);
             this.label2.TabIndex = 26;
@@ -87,7 +93,6 @@
             this.txtCodigo.Name = "txtCodigo";
             this.txtCodigo.Size = new System.Drawing.Size(275, 26);
             this.txtCodigo.TabIndex = 25;
-            this.txtCodigo.Text = "6666666666666666666666";
             // 
             // label1
             // 
@@ -103,18 +108,92 @@
             // 
             this.btnAdd.BackColor = System.Drawing.Color.SeaGreen;
             this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(122, 302);
+            this.btnAdd.Location = new System.Drawing.Point(536, 228);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(91, 34);
             this.btnAdd.TabIndex = 28;
             this.btnAdd.Text = "ADD";
             this.btnAdd.UseVisualStyleBackColor = false;
             // 
+            // txtNivel
+            // 
+            this.txtNivel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNivel.Location = new System.Drawing.Point(37, 233);
+            this.txtNivel.Multiline = true;
+            this.txtNivel.Name = "txtNivel";
+            this.txtNivel.Size = new System.Drawing.Size(275, 26);
+            this.txtNivel.TabIndex = 30;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 20);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "NIVEL DE CALIDAD:";
+            // 
+            // cmbCasas
+            // 
+            this.cmbCasas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCasas.FormattingEnabled = true;
+            this.cmbCasas.Location = new System.Drawing.Point(341, 235);
+            this.cmbCasas.Name = "cmbCasas";
+            this.cmbCasas.Size = new System.Drawing.Size(179, 24);
+            this.cmbCasas.TabIndex = 31;
+            // 
+            // btnTerminar
+            // 
+            this.btnTerminar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnTerminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTerminar.Location = new System.Drawing.Point(82, 343);
+            this.btnTerminar.Name = "btnTerminar";
+            this.btnTerminar.Size = new System.Drawing.Size(167, 62);
+            this.btnTerminar.TabIndex = 32;
+            this.btnTerminar.Text = "TERMINAR";
+            this.btnTerminar.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(337, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 20);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "CASAS:";
+            // 
+            // listCasas
+            // 
+            this.listCasas.Location = new System.Drawing.Point(341, 297);
+            this.listCasas.Name = "listCasas";
+            this.listCasas.Size = new System.Drawing.Size(239, 147);
+            this.listCasas.TabIndex = 34;
+            this.listCasas.UseCompatibleStateImageBehavior = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(337, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(185, 20);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "LISTADO DE CASAS:";
+            // 
             // AddTareas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(345, 369);
+            this.ClientSize = new System.Drawing.Size(639, 470);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listCasas);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnTerminar);
+            this.Controls.Add(this.cmbCasas);
+            this.Controls.Add(this.txtNivel);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtDescripcion);
             this.Controls.Add(this.label2);
@@ -142,5 +221,12 @@
         private System.Windows.Forms.TextBox txtCodigo;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtNivel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbCasas;
+        private System.Windows.Forms.Button btnTerminar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listCasas;
+        private System.Windows.Forms.Label label5;
     }
 }
